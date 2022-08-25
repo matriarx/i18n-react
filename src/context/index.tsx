3c8b59254474291b978359127fd640138a8ec1f4
Mia Matriarx <mia@matriarx.io>
2022-08-25 21:38:09 +0200

    style (#30)

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -21,29 +21,29 @@ import type {ReactNode} from 'react'
 
 export interface LocaleContext {
   locale: Intl.Locale
-  setLocale: (locale: Intl.Locale) => void
-  minimize: () => void
-  maximize: () => void
+  setLocale: (locale: Intl.Locale) => Promise<void>
+  minimize: () => Promise<void>
+  maximize: () => Promise<void>
   baseName: string
-  setBaseName: (baseName: string) => void
+  setBaseName: (baseName: string) => Promise<void>
   calendar: Calendar
-  setCalendar: (calendar: Calendar) => void
+  setCalendar: (calendar: Calendar) => Promise<void>
   hourCycle: Intl.LocaleHourCycleKey
-  setHourCycle: (hourCycle: Intl.LocaleHourCycleKey) => void
+  setHourCycle: (hourCycle: Intl.LocaleHourCycleKey) => Promise<void>
   region: Region
-  setRegion: (region: Region) => void
+  setRegion: (region: Region) => Promise<void>
   language: Language
-  setLanguage: (language: Language) => void
+  setLanguage: (language: Language) => Promise<void>
   script: Script
-  setScript: (script: Script) => void
+  setScript: (script: Script) => Promise<void>
   collation: Collation
-  setCollation: (collation: Collation) => void
+  setCollation: (collation: Collation) => Promise<void>
   caseFirst: Intl.LocaleCollationCaseFirst
-  setCaseFirst: (caseFirst: Intl.LocaleCollationCaseFirst) => void
+  setCaseFirst: (caseFirst: Intl.LocaleCollationCaseFirst) => Promise<void>
   numberingSystem: NumberingSystem
-  setNumberingSystem: (numberingSystem: NumberingSystem) => void
+  setNumberingSystem: (numberingSystem: NumberingSystem) => Promise<void>
   numeric: boolean
-  setNumeric: (numeric: boolean) => void
+  setNumeric: (numeric: boolean) => Promise<void>
   displayNames: (options: Intl.DisplayNamesOptions) => Intl.DisplayNames
   dateTimeFormat: (options: Intl.DateTimeFormatOptions) => Intl.DateTimeFormat
   relativeTimeFormat: (
@@ -61,12 +61,14 @@ export interface LocaleContext {
 export const LocaleContext = createContext<LocaleContext>({} as LocaleContext)
 
 export interface LocaleContextProvider {
-  locale?: Intl.Locale | undefined
-  translations?: Map<string, Map<string, string> | string> | undefined
+  locale: Intl.Locale | undefined
+  translations: Map<string, Map<string, string> | string> | undefined
   children: ReactNode
 }
 
-export const LocaleContextProvider = (props: LocaleContextProvider) => {
+export const LocaleContextProvider = (
+  props: LocaleContextProvider,
+): JSX.Element => {
   const [locale, setLocale] = useState<Intl.Locale>(
     getLocalStorageLocale() || props.locale || getSystemLocale(),
   )
@@ -106,59 +108,71 @@ export const LocaleContextProvider = (props: LocaleContextProvider) => {
 
   const value = {
     locale,
-    setLocale: (locale: Intl.Locale) => set(locale),
-    minimize: () => set(locale.minimize()),
-    maximize: () => set(locale.maximize()),
-    baseName: locale.baseName as string,
-    setBaseName: (baseName: string) => set(new Intl.Locale(locale, {baseName})),
+    setLocale: (locale: Intl.Locale): Promise<void> => set(locale),
+    minimize: (): Promise<void> => set(locale.minimize()),
+    maximize: (): Promise<void> => set(locale.maximize()),
+    baseName: locale.baseName,
+    setBaseName: (baseName: string): Promise<void> =>
+      set(new Intl.Locale(locale, {baseName})),
     calendar: locale.calendar as Calendar,
-    setCalendar: (calendar: Calendar) =>
+    setCalendar: (calendar: Calendar): Promise<void> =>
       set(new Intl.Locale(locale, {calendar})),
     hourCycle: locale.hourCycle as Intl.LocaleHourCycleKey,
-    setHourCycle: (hourCycle: Intl.LocaleHourCycleKey) =>
+    setHourCycle: (hourCycle: Intl.LocaleHourCycleKey): Promise<void> =>
       set(new Intl.Locale(locale, {hourCycle})),
     region: locale.region as Region,
-    setRegion: async (region: Region) => set(new Intl.Locale(locale, {region})),
+    setRegion: async (region: Region): Promise<void> =>
+      set(new Intl.Locale(locale, {region})),
     language: locale.language as Language,
-    setLanguage: async (language: Language) =>
+    setLanguage: async (language: Language): Promise<void> =>
       set(new Intl.Locale(locale, {language})),
     script: locale.script as Script,
-    setScript: async (script: Script) => set(new Intl.Locale(locale, {script})),
+    setScript: async (script: Script): Promise<void> =>
+      set(new Intl.Locale(locale, {script})),
     collation: locale.collation as Collation,
-    setCollation: (collation: Collation) =>
+    setCollation: (collation: Collation): Promise<void> =>
       set(new Intl.Locale(locale, {collation})),
     caseFirst: locale.caseFirst as Intl.LocaleCollationCaseFirst,
-    setCaseFirst: (caseFirst: Intl.LocaleCollationCaseFirst) =>
+    setCaseFirst: (caseFirst: Intl.LocaleCollationCaseFirst): Promise<void> =>
       set(new Intl.Locale(locale, {caseFirst})),
     numberingSystem: locale.numberingSystem as NumberingSystem,
-    setNumberingSystem: (numberingSystem: NumberingSystem) =>
+    setNumberingSystem: (numberingSystem: NumberingSystem): Promise<void> =>
       set(new Intl.Locale(locale, {numberingSystem})),
-    numeric: !!locale.numeric as boolean,
-    setNumeric: (numeric: boolean) => set(new Intl.Locale(locale, {numeric})),
-    displayNames: (options: Intl.DisplayNamesOptions) =>
+    numeric: !!locale.numeric,
+    setNumeric: (numeric: boolean): Promise<void> =>
+      set(new Intl.Locale(locale, {numeric})),
+    displayNames: (options: Intl.DisplayNamesOptions): Intl.DisplayNames =>
       new Intl.DisplayNames(locale.baseName, options),
-    dateTimeFormat: (options: Intl.DateTimeFormatOptions) =>
-      new Intl.DateTimeFormat(locale.baseName, options),
-    relativeTimeFormat: (options: Intl.RelativeTimeFormatOptions) =>
+    dateTimeFormat: (
+      options: Intl.DateTimeFormatOptions,
+    ): Intl.DateTimeFormat => new Intl.DateTimeFormat(locale.baseName, options),
+    relativeTimeFormat: (
+      options: Intl.RelativeTimeFormatOptions,
+    ): Intl.RelativeTimeFormat =>
       new Intl.RelativeTimeFormat(locale.baseName, options),
-    collator: (options: Intl.CollatorOptions) =>
+    collator: (options: Intl.CollatorOptions): Intl.Collator =>
       new Intl.Collator(locale.baseName, options),
-    listFormat: (options: Intl.ListFormatOptions) =>
+    listFormat: (options: Intl.ListFormatOptions): Intl.ListFormat =>
       new Intl.ListFormat(locale.baseName, options),
-    pluralRules: (options: Intl.PluralRulesOptions) =>
+    pluralRules: (options: Intl.PluralRulesOptions): Intl.PluralRules =>
       new Intl.PluralRules(locale.baseName, options),
-    segmenter: (options: Intl.SegmenterOptions) =>
+    segmenter: (options: Intl.SegmenterOptions): Intl.Segmenter =>
       new Intl.Segmenter(locale.baseName, options),
-    numberFormat: (options: Intl.NumberFormatOptions) =>
+    numberFormat: (options: Intl.NumberFormatOptions): Intl.NumberFormat =>
       new Intl.NumberFormat(locale.baseName, options),
-    translations: () =>
+    translations: (): Map<string, string> =>
       translations.get(locale.maximize().baseName) as Map<string, string>,
-    translate: (text: string) =>
-      translations
-        ? (
-            translations.get(locale.maximize().baseName) as Map<string, string>
-          ).get(text) || text
-        : text,
+    translate: (text: string): string => {
+      const baseName = locale.maximize().baseName
+
+      if (!translations || !translations.has(baseName)) {
+        return text
+      }
+
+      return (translations.get(baseName) as Map<string, string>).get(
+        text,
+      ) as string
+    },
   }
 
   return (
